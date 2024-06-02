07accb999b1c3a10e29b2dbfd63f0e24b18c626a
Wouter de Bruijn <wouter@wutru.io>
2024-06-02 18:11:22 +0200

    Accurate timing for cycles

--- ruboy_lib/src/lib.rs
+++ ruboy_lib/src/lib.rs
@@ -21,6 +21,11 @@ pub mod rom;
 
 pub use extern_traits::*;
 
+const TARGET_CLOCK_SPEED_HZ: u64 = 4194304;
+const SPEED_CHECK_INTERVAL_MS: u64 = 100;
+const CYCLES_PER_INTERVAL: u64 = (TARGET_CLOCK_SPEED_HZ * SPEED_CHECK_INTERVAL_MS) / 1000;
+const INTERVAL_DURATION: Duration = Duration::from_millis(SPEED_CHECK_INTERVAL_MS);
+
 pub struct Gameboy<A, R, V>
 where
     A: GBAllocator,
@@ -45,7 +50,16 @@ impl Frequency {
         Self::None(val).upcast()
     }
 
-    pub fn val(self) -> f64 {
+    pub fn val_raw(self) -> f64 {
+        match self {
+            Frequency::None(x) => x,
+            Frequency::Kilo(x) => x * 1000.0,
+            Frequency::Mega(x) => x * (1000.0 * 1000.0),
+            Frequency::Giga(x) => x * (1000.0 * 1000.0 * 1000.0),
+        }
+    }
+
+    pub fn val_unit(self) -> f64 {
         match self {
             Frequency::None(x) => x,
             Frequency::Kilo(x) => x,
@@ -54,7 +68,7 @@ impl Frequency {
         }
     }
     pub fn upcast(self) -> Self {
-        if self.val() > 1000.0 {
+        if self.val_unit() > 1000.0 {
             match self {
                 Frequency::None(x) => Frequency::Kilo(x / 1000.0).upcast(),
                 Frequency::Kilo(x) => Frequency::Mega(x / 1000.0).upcast(),
@@ -70,10 +84,10 @@ impl Frequency {
 impl Display for Frequency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            Frequency::None(x) => write!(f, "{:.2}Hz", x),
-            Frequency::Kilo(x) => write!(f, "{:.2}KHz", x),
-            Frequency::Mega(x) => write!(f, "{:.2}MHz", x),
-            Frequency::Giga(x) => write!(f, "{:.2}GHz", x),
+            Frequency::None(x) => write!(f, "{:.6}Hz", x),
+            Frequency::Kilo(x) => write!(f, "{:.6}KHz", x),
+            Frequency::Mega(x) => write!(f, "{:.6}MHz", x),
+            Frequency::Giga(x) => write!(f, "{:.6}GHz", x),
         }
     }
 }
@@ -106,7 +120,7 @@ impl<A: GBAllocator, R: RomReader, V: GBGraphicsDrawer> Gameboy<A, R, V> {
         log::info!("Starting Ruboy Emulator");
 
         let mut cycles = 0_usize;
-        let mut last_second = Instant::now();
+        let mut last_speed_check = Instant::now();
 
         loop {
             self.cpu.run_cycle(&mut self.mem)?;
@@ -114,13 +128,18 @@ impl<A: GBAllocator, R: RomReader, V: GBGraphicsDrawer> Gameboy<A, R, V> {
 
             cycles += 1;
 
-            let elapsed_time = last_second.elapsed();
-            if elapsed_time > Duration::from_secs(1) {
+            let elapsed_time = last_speed_check.elapsed();
+
+            if elapsed_time >= INTERVAL_DURATION {
                 let cycles_per_second = Frequency::new(cycles as f64 / elapsed_time.as_secs_f64());
 
                 log::info!("Current speed: {}", cycles_per_second);
                 cycles = 0;
-                last_second = Instant::now();
+                last_speed_check = Instant::now();
+            } else if cycles >= CYCLES_PER_INTERVAL as usize {
+                let wake_time = last_speed_check + INTERVAL_DURATION;
+
+                spin_sleep::sleep(wake_time.duration_since(Instant::now()));
             }
         }
     }
